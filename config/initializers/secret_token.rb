42372ae200e3025428ad17d45d3ec7e5db209c80
Eric Mooney <eric.c.mooney@gmail.com>
2013-08-02 13:34:01 -0700

    Initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrivialGame::Application.config.secret_token = 'fbc566d21395d24d2ad153588653b7d557a70e5398647aee44da48b9ae507f41ea7bd9968d461b04c377446167d195a35b76a8f7013de3a234d2efd4b0173f28'
