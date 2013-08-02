5d3ecfca4258782d54520ed0425223021c75274f
jason thein <emailmejason@yahoo.com>
2013-08-02 15:59:13 -0700

    merged and cleaned up via @ericmooney 's requirements

--- spec/features/integration_test_spec.rb
+++ spec/features/integration_test_spec.rb
@@ -6,10 +6,6 @@ describe "integration test", js: true do
     click_link "#new_game"
   end
 
-  before do
-    visit root_path
-  end
-
   it "Question loads at the beginning of round" do
 
    keydown("A")
@@ -59,6 +55,34 @@ describe "integration test", js: true do
     expect('#game_window').to have_css('.player1')
 
   end
+
+
+  it "activates player turn on key press" do
+    keydown("A")
+    expect(page).to have_css('background_color', 'red')
+  end
+
+  it "starts new round after incorrect answer" do
+    keydown("A")
+    click_div "#A2"
+
+    expect(page).to have_css('background_color', 'default')
+  end
+
+  it "increases players score on correct answer" do
+    keydown("A")
+    click_div "A1"
+    old_score = p1_score
+    expect(current_player_score).to_not be_equal(old_score)
+  end
+
+  it "increases the question counter when new question is loaded" do
+    keydown("A")
+    click_div "A1"
+    old_question_number = session_question_number
+    expect(current_question_number).to_not be_equal(old_question_number)
+  end
+
 
 
 end
