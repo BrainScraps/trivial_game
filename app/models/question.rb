0bfebeac288b7651f58400e6094de38a186b2d99
Eric Mooney <eric.c.mooney@gmail.com>
2013-08-02 14:39:08 -0700

    created and passed model tests

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ActiveRecord::Base
+  has_many :answers
   attr_accessible :content
+
+  validates_presence_of :content
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "gets created" do
+    question = Question.create(:content => "What color is the sun?")
+
+    expect(Question.all).to include(question)
+  end
+
+  it "needs content" do
+    question = Question.create()
+
+    expect(Question.all).not_to include(question)
+  end
+
+  it "has answers" do
+    question = Question.create(:content => "What color is the sun?")
+
+    answer = question.answers.create(:content => "yellow", :is_correct => true)
+    expect(Question.last.answers).to include(answer)
+  end
+
 end
